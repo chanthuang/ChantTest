6656e54d4c3002497a02929e3262405adba4f076
chantchen <chantchen@tencent.com>
2017-03-24 21:41:49 +0800

    一些测试代码

--- app/src/main/java/com/chant/chanttest/coordinator/CoordinatorActivity.java
+++ app/src/main/java/com/chant/chanttest/coordinator/CoordinatorActivity.java
@@ -1,53 +1,73 @@
 package com.chant.chanttest.coordinator;
 
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomSheetBehavior;
-import android.support.design.widget.CoordinatorLayout;
-import android.util.DisplayMetrics;
-import android.util.Log;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.WindowManager;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 
-import com.chant.chanttest.R;
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class CoordinatorActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        CoordinatorLayout baseView = (CoordinatorLayout) LayoutInflater.from(this)
-                .inflate(R.layout.activity_coordinator, null, false);
-        setContentView(baseView);
-        View bottomView = findViewById(R.id.bottomView);
-        bottomView.getLayoutParams().height = (int) (getDisplayMetrics(this).heightPixels * .9f);
-        bottomView.setLayoutParams(bottomView.getLayoutParams());
-        BottomSheetBehavior<View> bottomSheetBehavior = BottomSheetBehavior.from(bottomView);
-        bottomSheetBehavior.setPeekHeight((int) (getDisplayMetrics(this).heightPixels * .4f));
-        bottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+        FrameLayout frameLayout = new FrameLayout(this);
+
+        ListView simpleListView = new ListView(this);
+        simpleListView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,
+                android.R.id.text1, newList(0, 200)));
+        frameLayout.addView(simpleListView, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+
+        BottomSheetLayout baseView = new BottomSheetLayout(this);
+        frameLayout.addView(baseView, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+
+        RecyclerView panelRecyclerView = new RecyclerView(this);
+        panelRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        panelRecyclerView.setAdapter(new RecyclerView.Adapter() {
             @Override
-            public void onStateChanged(@NonNull View bottomSheet, int newState) {
-                Log.i("chant", "[onStateChanged] newState=" + newState);
+            public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                TextView textView = (TextView) LayoutInflater.from(parent.getContext())
+                        .inflate(android.R.layout.simple_list_item_1, null, false);
+                return new ViewHolder(textView);
             }
 
             @Override
-            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
-                Log.i("chant", "[onSlide] slideOffset=" + slideOffset);
+            public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+                TextView textView = (TextView) holder.itemView;
+                textView.setText(String.valueOf(position));
+            }
+
+            @Override
+            public int getItemCount() {
+                return 100;
+            }
+
+            class ViewHolder extends RecyclerView.ViewHolder {
+
+                ViewHolder(View itemView) {
+                    super(itemView);
+                }
             }
         });
+        baseView.setContentView(panelRecyclerView);
+
+        setContentView(frameLayout);
     }
-    /**
-     * DisplayMetrics
-     * @return
-     */
-    public static DisplayMetrics getDisplayMetrics(Context context){
-        DisplayMetrics displayMetrics = new DisplayMetrics();
-        ((WindowManager) context.getApplicationContext().getSystemService(Context.WINDOW_SERVICE))
-                .getDefaultDisplay().getMetrics(displayMetrics);
-        return displayMetrics;
+
+    private List<Integer> newList(int from, int to) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int i = from; i < to; i++) {
+            arrayList.add(i);
+        }
+        return arrayList;
     }
 }
 
